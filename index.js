62304ca9ef2b09562ccd29414f4dec9fe6585449
Matej Nemcek <ybdaba@gmail.com>
2017-08-10 00:07:45 +0200

    total bytes counter from mikrotik data

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let subarray = []
 let purearray = []
+let bytes = []
+let sumBytes = 0
 let fs = require('fs');
 let array = fs.readFileSync('rawdata').toString().split("\n");
 for(i in array) {
     subarray.push(array[i].split(' '))
     purearray.push(subarray[i].filter(ip => ip !== '*'))
+
+    if (purearray[i][2] !== undefined) {
+      bytes.push(parseInt(purearray[i][2]))
+    }
+
+    if (typeof bytes[i] === 'number') {
+      sumBytes += bytes[i]
+    }
 }
 
-console.log(purearray[1]);
+console.log('totalBytes', sumBytes);
